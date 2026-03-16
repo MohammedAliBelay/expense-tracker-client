fb51b7bc7368cb7e3aed4776a36c964cc70db1f4
MohammedAliBelay <eng.mame17@gmail.com>
2026-03-16 15:38:27 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
